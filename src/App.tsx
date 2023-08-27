5090fbaa90c052cec802fa0c6d8f5db79801ff2b
trishal-singh <trishalsinghhcwheel@gmail.com>
2023-08-28 00:52:57 +0530

    responsive layout starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid ,GridItem, Show} from "@chakra-ui/react";
+
 
 function App() {
-  
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg : `"nav nav" "aside main"`
+    }}>
+
+    <GridItem w='1000px' area="nav" bg="red"> Nav</GridItem>
+    <Show above="lg">
+    <GridItem area="aside" bg="green">Aside</GridItem>
+    </Show>
+    <GridItem area="main" bg="blue">Main</GridItem>
+    
+    </Grid>
+  )
 }
 
-export default App
+export default App;
